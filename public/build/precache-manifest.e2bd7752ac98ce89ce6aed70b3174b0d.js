94f3653c30fc51c0f91f19a6e9ed264a2a9bff49
Erce Kalabalikoglu <ercekalabalikoglu@ErceKaloglusMBP.localdomain>
2019-01-24 18:11:52 +0000

    updated tests

---
+++ public/build/precache-manifest.e2bd7752ac98ce89ce6aed70b3174b0d.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "229c360febb4351a89df",
+    "url": "/static/js/runtime~main.229c360f.js"
+  },
+  {
+    "revision": "46d125efa426fd9d0bc7",
+    "url": "/static/js/main.46d125ef.chunk.js"
+  },
+  {
+    "revision": "4ee62a34fb0028aa0a8f",
+    "url": "/static/js/1.4ee62a34.chunk.js"
+  },
+  {
+    "revision": "46d125efa426fd9d0bc7",
+    "url": "/static/css/main.1c6634cf.chunk.css"
+  },
+  {
+    "revision": "4aef49f63591dd11a7ba8125ae1b1326",
+    "url": "/index.html"
+  }
+];
