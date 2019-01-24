347528735c205921e4448bfbb6f3f78a1f591b78
Erce Kalabalikoglu <ercekalabalikoglu@ErceKaloglusMBP.localdomain>
2019-01-24 13:09:04 +0000

    json file

--- server.js
+++ server.js
@@ -12,8 +12,11 @@ app.use(express.static('public'))
 app.get('*', (req, res) => {
 	res.sendFile(path.resolve(__dirname, 'public', 'index.html'))
 })
-
 const PORT = process.env.PORT || 5000
+
+app.get("/", function(req, res, next) {
+  res.json(jsonFile);
+});
 
 app.listen(PORT, () => {
     console.log(`server started on ${PORT}`);
